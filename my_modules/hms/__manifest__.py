b4321740b1dc287f0ea5413b4ae442f2f0adf62d
Youssef Rady <121230785+Youssef-Rady@users.noreply.github.com>
2023-03-24 05:31:00 +0200

    Add files via upload

---
+++ my_modules/hms/__manifest__.py
@@ -0,0 +1,13 @@
+{
+    'name': 'Hospital Management',
+    'version': '1.0.0',
+    'summary': 'this is hospital management',
+    'description': '''
+    hospital management created by youssef
+    ''',
+    'author': 'iti team',
+    'application': True,
+    'data': [
+        'views/hms_views.xml',
+    ]
+}
---
+++ my_modules/hms/models/patient_model.py
@@ -0,0 +1,15 @@
+from odoo import models, fields
+
+class PatientModel(models.Model):
+    _name = 'hms.patient'
+
+    firstname = fields.Char()
+    lastname = fields.Char()
+    birthday = fields.Date()
+    history = fields.Html()
+    cr_ratio = fields.Float()
+    blood_type = fields.Selection([('a+',"a+"),("b+","b+"),("c+","c+"),("o+","o+"),("a-","a-"),("b-","b-"),("c-","c-"),("o-","o-")])
+    bcr = fields.Boolean()
+    images = fields.Image()
+    adress = fields.Text()
+    age = fields.Integer()
